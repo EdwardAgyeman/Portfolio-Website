0f94297d0a022221985e67b0586f68671e45ce17
Edward Agyeman <Eddi@Edwards-MacBook-Pro.local>
2020-07-17 14:15:37 +0200

    portfolio website v1.0

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+function navEffect() {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("li a");
+
+    sections.forEach(section => {
+        section.addEventListener("mouseenter", function() {
+            const id = this.getAttribute("id");
+            const navActive = document.querySelector(`a[href="#${id}"]`);
+            navLinks.forEach(link => link.classList.remove("active"));
+            navActive.classList.add("active");
+        })
+    })
+}
+
+navEffect();
